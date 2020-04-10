f07e6d50aa3271d93795070e8f7c48a9318f5264
ammi3 <758217673@qq.com>
2020-04-10 20:16:49 +0800

    springcloud

---
+++ springcloud/springcloud-service-admin/src/test/java/com/demo/springcloud/service/admin/SpringcloudServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springcloud.service.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudServiceAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
